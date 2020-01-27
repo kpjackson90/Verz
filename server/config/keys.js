8d7fecb080236db80aedf732c851550ea37d8069
Kareem Phillip-Jackson <kareem@crazyegg.com>
2020-01-27 11:25:12 -0400

    update keys file

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,6 +2,6 @@ if (process.env.NODE_ENV === "production") {
   module.exports = require("./prod");
 } else if (process.env.NODE_ENV === "ci") {
   module.exports = require("./ci");
-} else {
+} else if (process.env.NODE_ENV === "development") {
   module.exports = require("./dev");
 }
